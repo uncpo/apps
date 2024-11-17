2e5af4bcfb39fa71b28c05dd4c2d2bbe0ac2b658
uncpo <unicompotecnologia@gmail.com>
2024-11-17 20:50:00 -0300

    Add files via upload

---
+++ Wupdate.ps1
@@ -0,0 +1,23 @@
+chcp 1252 | Out-Null
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+
+Write-Host "PROCESSO SENDO EXECUTADO, NAO FECHE ESTA JANELA..." -ForegroundColor White -BackgroundColor DarkGreen
+Write-Host "O SISTEMA IRA REINICIAR APOS O TERMINO..." -ForegroundColor White -BackgroundColor DarkGreen
+Write-Host "SE NAO, REINICIE PARA APLICAR AS ATUALIZACOES..." -ForegroundColor White -BackgroundColor DarkGreen
+
+
+Install-Module -name PSWindowsUpdate -force
+Import-Module PSWindowsUpdate
+
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -AutoReboot
+cmd.exe /c "del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt""
