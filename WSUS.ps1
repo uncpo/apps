6a141ed1a178790f3fb069664d2b89de04ab3dbd
uncpo <unicompotecnologia@gmail.com>
2024-11-17 22:29:18 -0300

    Add files via upload

---
+++ WSUS.ps1
@@ -0,0 +1,28 @@
+chcp 1252 | Out-Null
+New-Item -Path C:\Setup -ItemType directory | Out-Null
+
+
+cls
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host "PREPARANDO REQUISITOS..." -ForegroundColor White -BackgroundColor DarkGreen
+Write-Host ""
+Write-Host ""
+Write-Host ""
+
+Set-ExecutionPolicy Unrestricted 
+
+Invoke-RestMethod https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/PowerShell-7.4.6-win-x64.msi -OutFile C:\Setup\PowerShell-7.4.6-win-x64.msi
+
+Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\Setup\PowerShell-7.4.6-win-x64.msi" /q '
+
+
+Start-Process "pwsh.exe" -Wait -WindowStyle Maximized -ArgumentList '-command irm github.com/asheroto/winget-install/releases/download/5.0.4/winget-install.ps1 | iex'
+
+Start-Process "pwsh.exe" -Wait -WindowStyle Maximized -ArgumentList '-command irm github.com/uncpo/apps/releases/download/Apps/Wupdate.ps1 | iex'
+
+
